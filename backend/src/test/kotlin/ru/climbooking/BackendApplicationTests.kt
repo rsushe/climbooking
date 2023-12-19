7b3317ae5d507961ae4a6f580be70eb0c45e7f34
rsushe <sushenkoroman2003@gmail.com>
2023-12-19 22:19:51 +0300

    backend: initial commit

---
+++ backend/src/test/kotlin/ru/climbooking/BackendApplicationTests.kt
@@ -0,0 +1,12 @@
+package ru.climbooking
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
