8103d752d26dd72fbcfab8457bddda9e6661fddf
rsushe <sushenkoroman2003@gmail.com>
2024-01-05 14:24:24 +0300

    initial frontend commit

---
+++ frontend/src/components/TrainingPlaces.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <h1>Места для тренировок</h1>
+    <table>
+      <thead>
+      <tr>
+        <th>Название</th>
+        <th>Тип</th>
+        <th>Местоположение</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="place in places" :key="place.id">
+        <td>{{ place.name }}</td>
+        <td>{{ place.type }}</td>
+        <td>{{ place.location }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      places: []
+    };
+  },
+  created() {
+    this.fetchPlaces();
+  },
+  methods: {
+    async fetchPlaces() {
+      try {
+        const response = await axios.get('http://localhost:8080/v1/places');
+        this.places = response.data;
+      } catch (error) {
+        console.log(error);
+        // Обработка ошибок или отображение сообщения об ошибке
+        // Например: this.error = 'Ошибка загрузки данных.'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th, td {
+  padding: 8px;
+  text-align: left;
+  border: 1px solid #ddd;
+}
+
+th {
+  background-color: #f2f2f2;
+}
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+import TrainingPlaces from './components/TrainingPlaces.vue'
+import Tournaments from './components/Tournaments.vue'
+import Climbers from './components/Climbers.vue'
+
+const routes = [
+    { path: '/', redirect: '/training-places'},
+    { path: '/training-places', component: TrainingPlaces },
+    { path: '/tournaments', component: Tournaments },
+    { path: '/climbers', component: Climbers }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
