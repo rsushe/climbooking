67cf854bc80a577c04a829b04c9ac95b9d34e8ef
rsushe <sushenkoroman2003@gmail.com>
2024-01-06 17:47:37 +0300

    CLIMBOOKING-13 fix

--- backend/src/main/kotlin/ru/climbooking/configuration/WebSecurityConfig.kt
+++ backend/src/main/kotlin/ru/climbooking/configuration/WebSecurityConfig.kt
@@ -2,12 +2,13 @@ package ru.climbooking.configuration
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.cors.CorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -19,11 +20,11 @@ class WebSecurityConfig {
                 authorize("/v1/*", permitAll)
             }
             cors {
-                val configuration = CorsConfiguration()
-                configuration.allowedOrigins = listOf("http://127.0.0.1:5173")
-                configuration.allowedMethods = listOf("GET", "POST")
-                configurationSource = UrlBasedCorsConfigurationSource().apply {
-                    registerCorsConfiguration("/**", configuration)
+                configurationSource = CorsConfigurationSource {
+                    val configuration = CorsConfiguration().applyPermitDefaultValues()
+                    configuration.allowedOrigins = listOf("http://127.0.0.1:5173")
+                    configuration.addAllowedMethod(HttpMethod.PATCH)
+                    configuration
                 }
             }
         }
